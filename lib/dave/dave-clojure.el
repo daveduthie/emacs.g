3ff34f426e09e8679c21d719cb04b2f0fecfdd42
David Duthie <dave.a.duthie@gmail.com>
2022-11-21 15:02:55 +0200

    make cider play nice with eglot

--- lib/dave/dave-clojure.el
+++ lib/dave/dave-clojure.el
@@ -1,5 +1,15 @@
+(use-package clojure-mode
+  :defer t
+  :config
+  (setq clojure-toplevel-inside-comment-form t))
+
 (use-package inf-clojure
   :defer t)
+
+(use-package cider
+  :defer t
+  :config
+  (setq cider-xref-fn-depth 90))
 
 (use-package zprint-format
   :commands (zprint-format-buffer
