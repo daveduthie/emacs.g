83c917524d28e3b31e3d5b56ccc1750938f86df2
David Duthie <dave.a.duthie@gmail.com>
2022-11-21 15:03:09 +0200

    ui tweaks

--- lib/dave/dave-ui.el
+++ lib/dave/dave-ui.el
@@ -1,10 +1,14 @@
 (custom-set-faces
  '(default ((t (:inherit nil :height 140 :family "JetBrains Mono")))))
 
-(load-theme 'modus-operandi)
-;; (load-theme 'modus-vivendi)
+(use-package modus-themes
+  :init (load-theme 'modus-operandi)
+  :bind ("M-<f6>" . modus-themes-toggle))
 
 (use-package winner
   :hook (prog-mode . (lambda () (winner-mode t))))
+
+(use-package display-line-numbers
+  :hook prog-mode)
 
 (provide 'dave-ui)
