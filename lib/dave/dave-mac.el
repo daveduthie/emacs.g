52c7c16047d30c34bdb259848e8e595531d49a38
David Duthie <dave.a.duthie@gmail.com>
2023-01-09 08:35:31 +0200

    use baked-in exec-path-from-shell

--- lib/dave/dave-mac.el
+++ lib/dave/dave-mac.el
@@ -2,8 +2,4 @@
   (setq mac-command-modifier 'meta)
   (setq mac-option-modifier 'super))
 
-;; (use-package exec-path-from-shell)
-;; (when (memq window-system '(mac ns x))
-;;   (exec-path-from-shell-initialize))
-
 (provide 'dave-mac)
