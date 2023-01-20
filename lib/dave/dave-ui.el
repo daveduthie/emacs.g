762830bcb1d319b3c99a7abf93b438a6466d4684
David Duthie <dave.a.duthie@gmail.com>
2023-01-20 17:29:08 +0200

    switch back to modus operandi

--- lib/dave/dave-ui.el
+++ lib/dave/dave-ui.el
@@ -1,12 +1,13 @@
 (custom-set-faces
  '(default ((t (:inherit nil :height 140 :family "Iosevka SS08")))))
 
-;; (use-package modus-themes
-;;   :init (load-theme 'modus-vivendi)
-;;   :bind ("M-<f6>" . modus-themes-toggle))
+(use-package modus-themes
+  :init (load-theme 'modus-operandi)
+  :bind ("M-<f6>" . modus-themes-toggle))
 
 (use-package doom-themes
-  :init (load-theme 'doom-nord-aurora 'no-confirm))
+  ;; :init (load-theme 'doom-nord 'no-confirm)
+  )
 
 (use-package winner
   :hook prog-mode)
