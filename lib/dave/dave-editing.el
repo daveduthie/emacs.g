e58070d12b3e8dcd4aaad790a08190f0825d424a
David Duthie <dave.a.duthie@gmail.com>
2023-05-15 09:35:39 +0200

    configure embark

--- lib/dave/dave-editing.el
+++ lib/dave/dave-editing.el
@@ -27,12 +27,39 @@
   :init (vertico-mode))
 
 (use-package consult
-  :defer 1
+  :defer 2
   :init
   (evil-define-key 'normal 'global (kbd "<SPC> s p") 'consult-ripgrep)
   (evil-define-key 'normal 'global (kbd "<SPC> s l") 'consult-line)
   (evil-define-key 'normal 'global (kbd ", e") 'consult-flymake)
   (evil-define-key 'normal 'global (kbd ", i") 'consult-imenu))
+
+(use-package embark
+  :defer t
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ;; ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  ;; Show the Embark target at point via Eldoc.  You may adjust the Eldoc
+  ;; strategy, if you want to see the documentation from multiple providers.
+  (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+
+  :config
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+(use-package embark-consult
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
 
 (use-package orderless
   :defer 2
