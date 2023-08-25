58b415a6dae76a4cb212f5f1a69bbd38c32986c7
David Duthie <dave.a.duthie@gmail.com>
2023-08-25 08:58:55 +0200

    Configure exec path from shell

--- lib/dave/dave-system.el
+++ lib/dave/dave-system.el
@@ -1,4 +1,12 @@
 (use-package envrc-mode
   :defer 3)
 
+(use-package exec-path-from-shell
+  :defer 1
+  :init
+  (setq exec-path-from-shell-arguments nil) ; don't use an interactive shell (i.e. load only .zshenv)
+  (when (or (memq window-system '(mac ns x))
+	    (daemonp))
+    (exec-path-from-shell-initialize)))
+
 (provide 'dave-system)
